ecae65b98c54c7e9f1edefbf99b793cdf452e8dc
João Victor <jvictor.franci@gmail.com>
2022-06-20 17:02:01 -0300

    change template sign-up

--- src/view/signup/index.js
+++ src/view/signup/index.js
@@ -1,178 +1,199 @@
-import axios from "axios";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
+import "./../../assets/css/fonts.css";
+import UserContext from "./../../assets/contexts/userContext.js";
 
-export function SignUp() {
-    const [loading, setLoading] = useState(false);
-    const [registerUser, setregisterUser] = useState({
-        email: "",
-        password: "",
-        confirmPassword: "",
-        username: "",
-        urlpicture: ""
+export default function SignInScreen() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const { loading, setLoading, setToken } = useContext(UserContext);
+
+  let tokenObject = localStorage.getItem("tokenUser");
+
+  const navigate = useNavigate();
+
+  const URL = "https://linkr-projeto17.herokuapp.com/";
+
+  useEffect(() => {
+    if (tokenObject) {
+      setToken({ ...JSON.parse(tokenObject) });
+      navigate("/timeline");
+    }
+  }, [setToken, navigate, tokenObject]);
+
+  function updateUser(event) {
+    const { name, value } = event.target;
+    setUser((prevState) => ({ ...prevState, [name]: value }));
+  }
+
+  function signInUser(event) {
+    event.preventDefault();
+    setLoading(true);
+    const promise = axios.post(`${URL}sign-in`, user);
+    promise.then(({ data }) => {
+      const newToken = data.token;
+      tokenObject = JSON.stringify(data);
+      localStorage.setItem("tokenUser", tokenObject);
+      setToken({ token: newToken });
+      setLoading(false);
+      navigate("/timeline");
     });
-    const navigate = useNavigate();
+    promise.catch((error) => {
+      alert(error.response.data);
+      setLoading(false);
+    });
+  }
 
-
-    function register(registerUser) {
-        setLoading(true);
-        if (registerUser.password !== registerUser.confirmPassword) {
-            alert("Senhas diferentes!");
-            setregisterUser({ ...registerUser, password: "", confirmPassword: "" });
-            return;
-        }
-
-        const promise = axios.post("http://localhost:4000/signup", registerUser);
-        promise.then(() => {
-            alert("deu bom!");
-            navigate("/");
-        });
-        promise.catch((err) => {
-            alert('erro')
-            console.log(err.response.message);
-            setLoading(false)
-        });
-    }
-
-    function sendUser(e) {
-        e.preventDefault();
-        register(registerUser);
-    }
-
-    return (
-        <MainStyle>
-            {loading ? <></> : 
-            <>
-            <h1>Linkr</h1>
-            <form
-                onSubmit={(e) => {
-                    sendUser(e);
-                }}
-            >
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={registerUser.email}
-                    disabled = {loading}
-                    onChange={(e) =>
-                        setregisterUser({ ...registerUser, email: e.target.value })
-                    }
-                    required
-                />
-                <input
-                    type="password"
-                    placeholder="Senha"
-                    value={registerUser.password}
-                    disabled = {loading}
-                    onChange={(e) =>
-                        setregisterUser({ ...registerUser, password: e.target.value })
-                    }
-                    required
-                />
-                <input
-                    type="password"
-                    placeholder="Confirme a senha"
-                    value={registerUser.confirmPassword}
-                    disabled = {loading}
-                    onChange={(e) => setregisterUser({ ...registerUser, confirmPassword: e.target.value })}
-                    required
-                />
-
-                <input
-                    type="text"
-                    placeholder="username"
-                    value={registerUser.username}
-                    onChange={(e) => setregisterUser({ ...registerUser, username: e.target.value })}
-                    required
-                />
-
-                <input
-                    type="text"
-                    placeholder="url Picture"
-                    value={registerUser.urlpicture}
-                    onChange={(e) => setregisterUser({ ...registerUser, urlpicture: e.target.value })}
-                    required
-                />
-
-
-                <button type="submit">Cadastrar</button>
-            </form>
-            <Link to="/">Já tem uma conta? Entre agora!</Link>
-            </>
-}
-        </MainStyle>
-    );
+  return (
+    <SignInScreenContainer>
+      <header>
+        <h1>linkr</h1>
+        <h2>save, share and discover the best links on the web</h2>
+      </header>
+      <StyledForm onSubmit={signInUser}>
+        <input
+          name="email"
+          type="email"
+          disabled={loading}
+          placeholder="e-mail"
+          value={user.email}
+          onChange={updateUser}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          disabled={loading}
+          placeholder="password"
+          value={user.password}
+          onChange={updateUser}
+          required
+        />
+        <button type="submit">Log In</button>
+        <StyledLink to="/sign-up">First time? Create an account!</StyledLink>
+      </StyledForm>
+    </SignInScreenContainer>
+  );
 }
 
-const MainStyle = styled.main`
-display: flex;
-width: 100%;
-height: 100vh;
-background-color:  #fa9a39;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-h1 {
-    font-weight: bold;
-    color: #ffffff;
-    font-size: 32px;
-    line-height: 50px;
-    margin: -5px 0px 24px;
-}
-h2 {
-    font-weight: bold;
-    color: #ffffff;
-    font-size: 25px;
-    line-height: 50px;
-    margin: -5px 0px;
-}
-form {
+const SignInScreenContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #333;
+  ::placeholder {
+    color: #fff;
+  }
+  header {
+    width: 100%;
+    height: 175px;
     display: flex;
     flex-direction: column;
     align-items: center;
-    width: 100%;
-}
-button {
-    background-color: #c747fc;
-    border: none;
-    width: 31%;
-    height: 54px;
-    color: #ffffff;
-    font-weight: 700;
-    font-size: 20px;
-    margin-top: 10px;
-    margin-bottom: 36px;
+    background-color: #151515;
+    margin-bottom: 40px;
+    h1 {
+      color: #fff;
+      font-family: "Passion one", cursive;
+      font-weight: bold;
+      font-size: 76px;
+      line-height: 84px;
+      letter-spacing: 0.05em;
+    }
+    h2 {
+      color: #fff;
+      font-family: "Oswald", sans-serif;
+      font-weight: 700;
+      font-size: 23px;
+      line-height: 34px;
+      text-align: center;
+      margin: 0 16%;
+    }
+  }
+  @media (min-width: 600px) {
     display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 30px;
-}
-button:hover{
-    cursor: pointer;
-}
-button:disabled{
-    background-color: #a09da1;
-    color: #ffffff00;
-}
-input{
-    width: 60%;
-    max-width: 470px;
-    height: 58px;
-    padding: 18px 15px;
-    border-radius: 30px;
-    border: none;
-    color: #000000;
-    font-size: 12 px;
-    margin-bottom: 13px;
-}
-a {
-    color: #FFFFFF;
-    text-decoration: none;
+    height: 100vh;
+    header {
+      width: 70%;
+      height: 100vh;
+      display: block;
+      padding: 300px 10%;
+      h1 {
+        font-size: 106px;
+        line-height: 117px;
+      }
+      h2 {
+        font-size: 43px;
+        line-height: 64px;
+        text-align: left;
+        width: 448px;
+        margin: 0;
+      }
+    }
+  }
+`;
+
+const StyledForm = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 40px;
+  input {
+    font-family: "Oswald", sans-serif;
+    width: 100%;
+    font-size: 22px;
+    line-height: 33px;
     font-weight: 700;
-    font-size: 15px;
-    line-height: 18px;
-}
+    padding-left: 17px;
+    height: 55px;
+    border-radius: 6px;
+    border: none;
+    margin-bottom: 12px;
+    letter-spacing: 0.03em;
+  }
+  button {
+    font-family: "Oswald", sans-serif;
+    width: 100%;
+    height: 55px;
+    color: #fff;
+    background-color: #1877f2;
+    border: none;
+    border-radius: 6px;
+    font-size: 22px;
+    font-weight: 700;
+    line-height: 33px;
+    letter-spacing: 0.03em;
+    margin-bottom: 18px;
+    cursor: pointer;
+    &:disabled {
+      opacity: 0.5;
+    }
+  }
+  @media (min-width: 600px) {
+    width: 40%;
+    justify-content: center;
+    input,
+    button {
+      height: 65px;
+      font-size: 27px;
+      line-height: 40px;
+    }
+  }
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  color: #fff;
+  font-size: 17px;
+  line-height: 20px;
+  @media (min-width: 600px) {
+    font-size: 20px;
+    line-height: 24px;
+  }
 `;
